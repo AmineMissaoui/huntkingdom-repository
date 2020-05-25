f99261fb07a5e317cc79e61f0ed30c4563bd83c7
AmineMissaoui <amine.missaoui@dqlick.com>
2020-05-25 22:31:29 +0100

    update

--- src/huntkingdom/gui/HomeFXMLController.java
+++ src/huntkingdom/gui/HomeFXMLController.java
@@ -62,41 +62,9 @@ public class HomeFXMLController implements Initializable {
             
         labelUserName.setText(session.getUsername());
         
-        try {
-            ArrayList<User> users = new ServiceUser().getUsers();
-            ArrayList<Entreprise> entreprises = new ServiceUser().getEntreprise();
-            homePane.getChildren().add(showUsers(users));
-            homePaneEntreprise.getChildren().add(showEntreprises(entreprises));
-            //labelUserName.setText(UserSession.getUsername());
-        } catch (SQLException ex) {
-            System.out.println("error" + ex.getMessage());
-        }
+
     }
 
-    public VBox showUsers(ArrayList<User> users){
-        VBox liste = new VBox();
-        for(User u : users){
-            HBox graphicUser = new HBox();
-            graphicUser.getChildren().add(new Label(u.getUsername()));
-            graphicUser.getChildren().add(new Label(u.getFirst_name()));
-            graphicUser.getChildren().add(new Label(u.getLast_name()));
-            graphicUser.getChildren().add(new JFXToggleButton());
-            liste.getChildren().add(graphicUser);
-        }
-        //liste.prefHeightProperty().bind(homePane.heightProperty().multiply(0.5));
-        return liste;
-    }
-    public VBox showEntreprises(ArrayList<Entreprise> entreprises){
-        VBox liste = new VBox();
-        for(Entreprise e : entreprises){
-            HBox graphicUser = new HBox();
-            graphicUser.getChildren().add(new Label(e.getUsername()));
-            graphicUser.getChildren().add(new Label(e.getRaison_sociale()));
-            graphicUser.getChildren().add(new Label(e.getMatricule_fiscale()));
-            liste.getChildren().add(graphicUser);
-        }
-        //liste.prefHeightProperty().bind(homePane.heightProperty().multiply(0.5));
-        return liste;
-    }
+
     
 }
